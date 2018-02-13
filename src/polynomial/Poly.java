a10a08e11761df36f7a6a8a3dcc0a28fcdcf0266
makromant <makromant@gmail.com>
2018-02-13 19:51:51 +0300

    Created an Overloaded Constructor

--- src/polynomial/Poly.java
+++ src/polynomial/Poly.java
@@ -1,5 +1,13 @@
 package polynomial;
 
 public class Poly{
-	 
+	private final int[] variables;
+
+	Poly(int[] variables){
+		this.variables = variables;
+	}
+
+	public int[] getVariables(){
+		return variables;
+	}
 }
