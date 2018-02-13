222ca739037d561d595bbe0377dde142c15aac58
makromant <makromant@gmail.com>
2018-02-13 20:01:09 +0300

    Created a method for comparing two polynomials

--- src/polynomial/Poly.java
+++ src/polynomial/Poly.java
@@ -2,12 +2,32 @@ package polynomial;
 
 public class Poly{
 	private final int[] variables;
+	private final int length;
 
 	Poly(int[] variables){
 		this.variables = variables;
+		length = this.variables.length;
 	}
 
+	//@Override
+	public boolean equals(Poly obj){
+		int[] objVariables = obj.getVariables();
+		if (length == obj.getLength()){
+			for (int i = 0; i<length; i++){
+				if (variables[i] != objVariables[i]){
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	
 	public int[] getVariables(){
 		return variables;
+	}
+
+	public int getLength(){
+		return length;
 	}
 }
