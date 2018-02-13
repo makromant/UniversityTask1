bbba42722a8840c95646d4267c1172fd9c392d0c
makromant <makromant@gmail.com>
2018-02-13 21:33:10 +0300

    Created a method for dividing one polynomial into another and method for finding the remainder of the division. Corrected style

--- src/polynomial/Poly.java
+++ src/polynomial/Poly.java
@@ -8,7 +8,14 @@ public class Poly{
 		this.variables = variables;
 		length = this.variables.length;
 	}
-
+	
+	private void zeroException(int[] objVariables, int objLength){
+		if (objLength == 0 || (objLength == 1 && objVariables[0] == 0)
+									  || length == 0 || (length == 1 && variables[0] == 0)){
+			throw new  ArithmeticException("");
+		} 
+	}
+	
 	//@Override
 	public boolean equals(Poly obj){
 		int[] objVariables = obj.getVariables();
@@ -28,7 +35,7 @@ public class Poly{
 		int power = 0;
 		long polyValue = 0;
 		for (int element:variables){
-			polyValue += Math.pow(x, power)*element;
+			polyValue += Math.pow(x, power) * element;
 			power++;
 		}
 		return polyValue;
@@ -55,7 +62,7 @@ public class Poly{
 		}
 		
 		if (larger != smaller){
-			for(int i = smaller; i<larger;i++){
+			for(int i = smaller; i < larger;i++){
 				newVariables[i] = largerVariables[i]; 
 			}
 	   	}
@@ -78,18 +85,56 @@ public class Poly{
 			smaller = length;
 		}
 		int[] newVariables = new int[larger];
-		for(int i = 0; i<smaller; i++){
+		for(int i = 0; i < smaller; i++){
 			newVariables[i] = variables[i] - objVariables[i];
 		}
 		
 		if (larger != smaller){
-			for(int i = smaller; i<larger;i++){
+			for(int i = smaller; i < larger;i++){
 				newVariables[i] = -largerVariables[i]; 
 			}
 	   	}
 		return new Poly(newVariables);
 	}
-	
+
+	private Poly div(Poly obj, boolean which){
+		int [] objVariables = obj.getVariables();
+		int objLength = obj.getLength();
+		zeroException(objVariables, objLength);
+		int[] divider;
+		int[] divisor;
+		if (length > objLength){
+		    divider = variables;
+			divisor = objVariables;
+		} else{
+			divider = objVariables;
+		    divisor = variables;
+		}
+		int[] remainder = divider;
+		int[] quotient = new int[divider.length - divisor.length + 1];
+		for (int i = 0; i < quotient.length;i++){
+			int coefficient = remainder[remainder.length - i - 1] / divisor[0];
+			quotient[quotient.length - i - 1] = coefficient;
+			for (int j = 0; j < divisor.length; j++){
+				remainder[remainder.length - i - j - 1] -= coefficient * divisor[divisor.length - j - 1];
+			}
+		}
+		if (which){
+			return new Poly(quotient);
+		}
+		else{
+			return new Poly(remainder);
+		}
+	}
+
+	public Poly division(Poly obj){
+		return div(obj, true);
+   	}
+
+	public Poly remainderOfDiv(Poly obj){
+		return div(obj, false);
+	}
+
 	public int[] getVariables(){
 		return variables;
 	}
@@ -97,4 +142,5 @@ public class Poly{
 	public int getLength(){
 		return length;
 	}
+	
 }
